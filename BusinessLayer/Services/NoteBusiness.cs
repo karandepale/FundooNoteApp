979e47e953ae3c12d0d60bf854b59c58abb0b65b
“karandepale” <karandepale111@gmail.com>
2023-08-26 15:34:01 +0530

    [Karan] ADD: Implement Business logic for create note inside business layer

---
+++ BusinessLayer/Services/NoteBusiness.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLayer.Interfaces;
+using CommonLayer.Model;
+using RepoLayer.Entity;
+using RepoLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Services
+{
+    public class NoteBusiness : INoteBusiness
+    {
+        private readonly INoteRepo noteRepo;
+        public NoteBusiness(INoteRepo noteRepo)
+        {
+            this.noteRepo = noteRepo;
+        }
+
+        public NoteEntity CreateNote(NoteCreateModel model)
+        {
+            try
+            {
+                return noteRepo.CreateNote(model);
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+
+    }
+}
