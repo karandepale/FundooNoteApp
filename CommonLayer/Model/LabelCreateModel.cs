72c8e5878ae33ad9ed4cc3c28c1f4a2b9247f67c
“karandepale” <karandepale111@gmail.com>
2023-08-30 16:49:13 +0530

    [Karan] Add : Added a Model for Create a label

---
+++ CommonLayer/Model/LabelCreateModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.Model
+{
+    public class LabelCreateModel
+    {
+        public string Title { get; set; }
+
+    }
+}
