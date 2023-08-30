ddebb87340044da6c9a86c8e5d7bd325b1fd3b7b
“karandepale” <karandepale111@gmail.com>
2023-08-30 15:06:58 +0530

    [Karan] Implement : Implemented the service and interface for create  Collab and done the logic for create a collab for a particular user

---
+++ RepoLayer/Services/CollabRepo.cs
@@ -0,0 +1,53 @@
+﻿using CommonLayer.Model;
+using RepoLayer.Context;
+using RepoLayer.Entity;
+using RepoLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepoLayer.Services
+{
+    public class CollabRepo : ICollabRepo
+    {
+        private readonly FundooContext fundooContext;
+        public CollabRepo(FundooContext fundooContext)
+        {
+            this.fundooContext = fundooContext;
+        }
+
+
+
+        // CREATE COLLAB:-
+        public CollabEntity CreateCollab(CollabCreateModel model, long NoteID , long userId)
+        {
+            try
+            {
+              
+                CollabEntity collabEntity = new CollabEntity();
+                collabEntity.Email = model.Email;
+                collabEntity.UserID = userId;
+                collabEntity.NoteID = NoteID;
+
+                fundooContext.Collab.Add(collabEntity);
+                fundooContext.SaveChanges();
+
+                if (collabEntity != null)
+                {
+                    return collabEntity;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+
+
+    }
+}
