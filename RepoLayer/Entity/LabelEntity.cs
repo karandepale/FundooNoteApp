87d945a1a6affb95436cb2a359dba698d3dca203
“karandepale” <karandepale111@gmail.com>
2023-08-30 16:34:46 +0530

    [Karan] Add : Added a model for Label Tablel

---
+++ RepoLayer/Entity/LabelEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace RepoLayer.Entity
+{
+    public class LabelEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long LabelID { get; set; }
+        public string Title { get; set; }
+
+
+
+        // Foreign key USERID from User table:-
+        [ForeignKey("User")]
+        public long UserID { get; set; }
+        [JsonIgnore]
+        public UserEntity User { get; set; }
+
+
+
+        // Foreign Key NoteID form Notes Table:-
+        [ForeignKey("Note")]
+        public long NoteID { get; set; }
+        [JsonIgnore]
+        public NoteEntity Note { get; set; }
+
+
+    }
+}
