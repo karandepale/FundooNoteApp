f223d6a2e3f894adca96196e329fe6f7ee6602c9
“karandepale” <karandepale111@gmail.com>
2023-08-30 16:58:12 +0530

    [Karan] Feature : Implement the feature of Create Label for a particular note

---
+++ FundooNoteApp/Controllers/LabelController.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLayer.Interfaces;
+using BusinessLayer.Services;
+using CommonLayer.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace FundooNoteApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LabelController : ControllerBase
+    {
+        private readonly ILabelBusiness labelBusiness;
+        public LabelController(ILabelBusiness labelBusiness)
+        {
+            this.labelBusiness = labelBusiness;
+        }
+
+
+        // CREATE LABEL:-
+        [Authorize]
+        [HttpPost]
+        [Route("CreateLabel")]
+        public IActionResult CreateLabel(LabelCreateModel model, long NoteID)
+        {
+            var userIdClaim = User.Claims.FirstOrDefault(u => u.Type == "UserID");
+            if (userIdClaim != null && long.TryParse(userIdClaim.Value, out long userId))
+            {
+                var result = labelBusiness.CreateLabel(model, NoteID);
+
+                if (result != null)
+                {
+                    return Ok(new { success = true, message = "Label Created Successful", data = result });
+                }
+                else
+                {
+                    return NotFound(new { success = false, message = "Label Creation Failed", data = result });
+                }
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+
+    }
+}
