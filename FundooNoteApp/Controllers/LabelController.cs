2477119aa48b6ad84538d392bc3e40171d615c94
“karandepale” <karandepale111@gmail.com>
2023-10-01 23:50:55 +0530

    [Karan] Implement : Redis Cache Implemented for Authorized Get All Labels for a particular note API

--- FundooNoteApp/Controllers/LabelController.cs
+++ FundooNoteApp/Controllers/LabelController.cs
@@ -4,8 +4,13 @@ using CommonLayer.Model;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using RepoLayer.Entity;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace FundooNoteApp.Controllers
 {
@@ -14,9 +19,11 @@ namespace FundooNoteApp.Controllers
     public class LabelController : ControllerBase
     {
         private readonly ILabelBusiness labelBusiness;
-        public LabelController(ILabelBusiness labelBusiness)
+        private readonly IDistributedCache distributedCache;
+        public LabelController(ILabelBusiness labelBusiness , IDistributedCache distributedCache)
         {
             this.labelBusiness = labelBusiness;
+            this.distributedCache = distributedCache;
         }
 
 
@@ -49,29 +56,75 @@ namespace FundooNoteApp.Controllers
 
 
         // GET ALL LABELS:-
+        /*  [Authorize]
+          [HttpGet]
+          [Route("GetAllLabels")]
+          public IActionResult GetAllLabel(long NoteID)
+          {
+              var userIdClaim = User.Claims.FirstOrDefault(u => u.Type == "UserID");
+              if (userIdClaim != null && long.TryParse(userIdClaim.Value, out long userId))
+              {
+                  var result = labelBusiness.GetAllLabels(NoteID);
+
+                  if (result != null)
+                  {
+                      return Ok(new { success = true, message = "Label Getting Successful", data = result });
+                  }
+                  else
+                  {
+                      return NotFound(new { success = false, message = "Unable to get Labels", data = result });
+                  }
+              }
+              else
+              {
+                  return Unauthorized();
+              }
+          }*/
+
+
+        // GET ALL LABELS USING RADDIS CACHE:-
         [Authorize]
         [HttpGet]
         [Route("GetAllLabels")]
-        public IActionResult GetAllLabel(long NoteID)
+        public async Task<IActionResult> GetLabels(long NoteID)
         {
-            var userIdClaim = User.Claims.FirstOrDefault(u => u.Type == "UserID");
-            if (userIdClaim != null && long.TryParse(userIdClaim.Value, out long userId))
-            {
-                var result = labelBusiness.GetAllLabels(NoteID);
+            var myKey = "labelList";
+            var serializeLabelList = await distributedCache.GetStringAsync(myKey);
+            List<LabelEntity> result;
 
-                if (result != null)
-                {
-                    return Ok(new { success = true, message = "Label Getting Successful", data = result });
-                }
-                else
-                {
-                    return NotFound(new { success = false, message = "Unable to get Labels", data = result });
-                }
+            if (serializeLabelList != null)
+            {
+                result = JsonConvert.DeserializeObject<List<LabelEntity>>(serializeLabelList);
             }
             else
             {
-                return Unauthorized();
+                var userIdClaim = User.Claims.FirstOrDefault(data => data.Type == "UserID");
+                if (userIdClaim != null && long.TryParse(userIdClaim.Value, out long userId))
+                {
+                    result = labelBusiness.GetAllLabels(NoteID);
+                    serializeLabelList = JsonConvert.SerializeObject(result);
+
+                    await distributedCache.SetStringAsync(myKey, serializeLabelList, new DistributedCacheEntryOptions
+                    {
+                        AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(10),
+                        SlidingExpiration = TimeSpan.FromMinutes(2)
+                    });
+                }
+                else
+                {
+                    return BadRequest(new { success = false, message = "Invalid user ID claim" });
+                }
             }
+
+            if (result != null)
+            {
+                return Ok(new { success = true, message = "List Of Labels getting Successful.", data = result });
+            }
+            else
+            {
+                return NotFound(new { success = false, message = "List Of Labels Not getting.", data = result });
+            }
+
         }
 
 
